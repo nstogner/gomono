afca05063334a322552ef05525883284790b3c9b
Nick Stogner <nstogner@redventures.com>
2016-11-12 18:48:46 -0500

    Add git repo object removal

---
+++ apply.sh
@@ -0,0 +1,5 @@
+cp pre-commit .git/hooks/pre-commit
+cp post-commit .git/hooks/post-commit
+cp post-merge .git/hooks/post-merge
+cp post-rewrite .git/hooks/post-rewrite
+cp post-checkout .git/hooks/post-checkout
